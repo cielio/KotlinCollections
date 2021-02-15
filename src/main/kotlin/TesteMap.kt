56bfcf2d2dfc3b57e38c2089a6da4a9c6b21ac32
cielio <cielion@hotmail.com>
2021-02-14 23:13:36 -0300

    Add testmap

---
+++ src/main/kotlin/TesteMap.kt
@@ -0,0 +1,13 @@
+fun main() {
+	val pair: Pair<String, Double> = Pair("Joao", 100.0)
+	val map1 = mapOf(pair)
+
+	map1.forEach { (t, u) -> println("Chave: $t - Valor: $u") }
+	val  map2 = mapOf(
+			"Pedro" to 200.0,
+			"Cielio" to 2030.0
+	)
+
+	map2.forEach { (t, u) -> println("Chave: $t - Valor: $u") }
+
+}
